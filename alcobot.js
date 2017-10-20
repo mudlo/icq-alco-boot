35679c9a56676f35ad0a3b904fcdffa9c285ee6d
a.makurin <makurin@corp.mail.ru>
2017-10-21 00:13:08 +0300

    init alcobot

--- alcobot.js
+++ alcobot.js
@@ -1,5 +1,28 @@
 var request = require('request');
+var uuid = require('uuid/v1');
 
 module.exports = function (req, res, next) {
-  
+  var botPayload = {};
+}
+
+function send (payload, callback) {
+  var uid = process.env.UID;
+  var sid = process.env.SID;
+  var to = process.env.TO;
+ 
+  request({
+    uri: 'https://botapi.icq.net/im/sendIM' +
+         '?aimsid=' + sid +
+         '&t=' + to +
+         '&r=' + uuid() +
+         '&message=kryjoopyl',
+    method: 'POST',
+    body: JSON.stringify(payload)
+  }, function (error, response, body) {
+    if (error) {
+      return callback(error);
+    }
+
+    callback(null, response.statusCode, body);
+  });
 }
