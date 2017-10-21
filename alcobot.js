dbba52d8c920ad9a4ef6e1269a8ddf1fc5c24422
a.makurin <makurin@corp.mail.ru>
2017-10-21 13:01:09 +0300

    init alcobot

--- alcobot.js
+++ alcobot.js
@@ -24,9 +24,10 @@ function send(method, payload, callback) {
     uri: url + method +
          '?aimsid=' + sid +
          '&t=' + to +
-         '&r=' + uuid() +
-         '&message=' + payload,
-    method: 'POST'
+         '&r=' + uuid()// +
+//         '&message=' + payload,
+    method: 'POST',
+    message: payload
   }, function (error, response, body) {
     if (error) {
       return callback(error);
